f81e356024bdeeeca68f2a7ae5081dd6f0fa5352
Antoine BERNIER <antoine.bernier@gmail.com>
2023-01-09 00:55:54 +0100

    tweaks

--- server.js
+++ server.js
@@ -1,30 +1,31 @@
-import { resolve } from "path";
-
 import express from "express";
 import csv from "csvtojson";
 
 const app = express();
 
-const filePath = resolve("./urls.csv");
-const jsonArray = await csv().fromFile(filePath);
-console.log(jsonArray);
+//
+// DB (from CSV)
+//
 
-const map = new Map(jsonArray.map(({ short, original }) => [short, original]));
+const jsonArray = await csv().fromFile("./urls.csv");
+const db = new Map(jsonArray.map(({ short, original }) => [short, original]));
+
+//
+// all-route
+//
 
 app.get("*", async (req, res, next) => {
   const short = req.url;
-  // console.log("short", short);
 
-  const exist = map.has(short);
+  const exist = db.has(short);
+  if (!exist) return next(); // Not found
 
-  if (!exist) return next(); // not found
-
-  // 301
-  const original = map.get(short);
+  // Redirection
+  const original = db.get(short);
   res.redirect(original);
 });
 
-// 404 middleware
+// 404
 app.use((req, res, next) => {
   res
     .status(404)
@@ -34,6 +35,4 @@ app.use((req, res, next) => {
 });
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+app.listen(port, () => console.log(`Server is running on port ${port}`));
